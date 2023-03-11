12e544aa6f270479b67bf4f3ff5e3d1c59692bc9
rimhajal <rim.hajal@gmail.com>
2023-03-11 14:35:27 +0100

    only putting 1s around the mine

--- R/matrixBase.R
+++ R/matrixBase.R
@@ -1,48 +1,57 @@
-getNeighbors <- function(row, col, rows, cols){
-  neighbors <- list()
-  if (row > 0){  #UP
-    neighbors <- append(neighbors, c(row-1,col))
-  }
-  if (row < rows-1){   #DOWN
-    neighbors <- append(neighbors, c(row+1,col))
-  }
-  if (col > 0){    #LEFT
-    neighbors <- append(neighbors, c(row,col-1))
-  }
-  if (col < cols-1){    #RIGHT
-    neighbors <- append(neighbors, c(row,col+1))
-  } 
-  #DIAGONALS 
-  if (row > 0 & col > 0){
-    neighbors <- append(neighbors, c(row-1,col-1))
-  }
-  if (row < rows-1 & col < cols-1){
-    neighbors <- append(neighbors, c(row+1,col+1))
-  }
-  if (row < rows-1 & col > 0){
-    neighbors <- append(neighbors, c(row+1,col-1))
-  }
-  if (row > 0 & col < cols-1){
-    neighbors <- append(neighbors, c(row-1,col+1))
-  }
+#allows us to add a list inside another list
+#each list will serve as positions in the future matrix
+lappend <- function (lst, ...){
+  lst <- c(lst, list(...))
+  return(lst)
 }
 
-firstGrid <- function(w, l){
-  grid <- matrix(data = NA, nrow = l, ncol = w)
-  randomMines <- sample(nrow(grid)*ncol(grid), runif(1,min=1,max=15))
+#identifies all the neighboring squares in order to number out the mines later on
+getNeighbors <- function(i, j, rows, cols){
+  neighbors <- list()
+  if (i > 1){  #UP
+    neighbors <- lappend(neighbors, c(i-1,j))
+  }
+  if (i < rows){   #DOWN
+    neighbors <- lappend(neighbors, c(i+1,j))
+  }
+  if (j > 1){    #LEFT
+    neighbors<- lappend(neighbors, c(i,j-1))
+  }
+  if (j < cols){    #RIGHT
+    neighbors<- lappend(neighbors, c(i,j+1))
+  } 
+  #DIAGONALS 
+  if (i > 1 & j > 1){
+    neighbors<- lappend(neighbors, c(i-1,j-1))
+  }
+  if (i < rows & j < cols){
+    neighbors<- lappend(neighbors, c(i+1,j+1))
+  }
+  if (i < rows & j > 1){
+    neighbors<- lappend(neighbors, c(i+1,j-1))
+  }
+  if (i > 1 & j < cols){
+    neighbors<- lappend(neighbors, c(i-1,j+1))
+  }
+  neighbors
+}
+
+#randomly places mines with corresponding number around the mine
+firstGrid <- function(w, l, nbofMines){
+  grid <- matrix(data = 0, nrow = l, ncol = w)
+  randomMines <- sample(nrow(grid)*ncol(grid), runif(1,min=1,max=nbofMines))
   grid[randomMines] <- -1
   for (i in 1:l){
     for (j in 1:w){
-      if (!is.na(grid[i][j])){
+      if (isTRUE(grid[i,j] == -1)) {
         neighbors <- getNeighbors(i,j,l,w)
       }
+      for (k in neighbors){
+        grid[k[1],k[2]] <- 1
       }
+    }
   }
-  for (k in neighbors){
-    grid[k] <- +1
-  }
-grid
+  grid
 }
 
-firstGrid(10,10)
-  
+
