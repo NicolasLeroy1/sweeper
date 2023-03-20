f050f32e463fde69a76a3bce1f0a453dffdf07ed
rimhajal <rim.hajal@gmail.com>
2023-03-20 16:19:18 +0100

    trying a new approach

--- R/addingLogo.R
+++ R/addingLogo.R
@@ -1,9 +1,9 @@
 library(shiny)
 
 hiddenSquareUI <- function(i, j) {
-  tags$image(
-    href = sprintf("logo/icons8-data-grid-24.png", "hidden"),
-    class = "hiddenSquare",
+  tags$img(
+    src = "/logo/icons8-data-grid-24.png",
+    alt = "hiddenSquare",
     x = j,
     y = i,
     width = 1,
@@ -12,9 +12,9 @@ hiddenSquareUI <- function(i, j) {
 }
 
 openedSquareUI <- function(i, j) {
-  tags$image(
-    href = sprintf("logo/icons8-data-grid-24 (1).png", "played"),
-    class = "playedSquare",
+  tags$img(
+    src = "/logo/icons8-data-grid-24 (1).png",
+    alt = "playedSquare",
     x = j,
     y = i,
     width = 1,
@@ -23,9 +23,9 @@ openedSquareUI <- function(i, j) {
 }
 
 flagLogoUI <- function(i, j) {
-  tags$image(
-    href = sprintf("logo/icons8-empty-flag-30.png", "flagged"),
-    class = "flagged",
+  tags$img(
+    src = "/logo/icons8-empty-flag-30.png",
+    alt = "flagged",
     x = j,
     y = i,
     width = 1,
@@ -34,12 +34,95 @@ flagLogoUI <- function(i, j) {
 }
 
 mineLogoUI <- function(i, j) {
-  tags$image(
-    href = sprintf("logo/icons8-naval-mine-30.png", "mines"),
-    class = "mines",
+  tags$img(
+    src = "/logo/icons8-naval-mine-30.png",
+    alt = "mines",
     x = j,
     y = i,
     width = 1,
     height = 1
   )
 }
+
+drawGridUI <- function(inputId, l, w, status,fun) {
+  cells = apply(expand.grid(1:l, 1:w), 1, function(index) {
+    i = index[1]
+    j = index[2]
+    
+    fun(i,j)
+  })
+  
+  tagList(
+    singleton(tags$head(tags$script(jscode))),
+    tags$svg(
+      `data-input-id` = inputId,
+      class = sprintf("minesweeper-grid %s", status),
+      width = 25 * w,
+      viewBox = sprintf("1 1 %d %d", w, l),
+      tagList(cells)
+    )
+  )
+}
+
+
+ongoingGridUI <- function(inputId, grid, state) {
+  drawGridUI(
+    inputId = inputId,
+    l = grid$l,
+    w = grid$w,
+    status = "ongoing",
+    function(i, j) {
+      if (state$played[i, j]) {
+        openedSquareUI(i, j, grid$neighbors[i, j])
+      } else if (state$hidden[i, j]) {
+        flagLogoUI(i, j)
+      } else {
+        hiddenSquareUI(i, j)
+      }
+    }
+  )
+}
+
+
+youLoseUI <- function(grid, state) {
+  drawGridUI(
+    inputId = NULL,
+    l = grid$l,
+    w = grid$w,
+    status = "You lose !",
+    function(i, j) {
+      if (state$revealed[i, j] && game$mines[i, j]) {
+        flagLogoUI(i, j)
+      } else if (state$played[i, j]) {
+        openedSquareUI(i, j, grid$neighbors[i, j])
+      } else if (state$hidden[i, j] && !grid$mines[i, j]) {
+        mineLogoUI(i, j)
+      } else if (state$hidden[i, j]) {
+        flagLogoUI(i, j)
+      } else if (grid$mines[i, j]) {
+        mineLogoUI(i, j)
+      } else {
+        hiddenSquareUI(i, j)
+      }
+    }
+  )
+}
+
+
+youWinUI <- function(grid) {
+  drawGridUI(
+    inputId = NULL,
+    l = grid$l,
+    w = grid$w,
+    status = "You win !",
+    function(i, j) {
+      if (grid$mines[i, j]) {
+        flagLogoUI(i, j)
+      } else {
+        openedSquareUI(i, j, grid$neighbors[i, j])
+      }
+    }
+  )
+}
+
+
