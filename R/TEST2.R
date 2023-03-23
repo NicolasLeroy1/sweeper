946da98cde2d4daf9fece4898914ca0988816b92
NicolasLeroy1 <nicolas.leroy@etu.umontpellier.fr>
2023-03-23 09:21:43 +0100

    Update TEST2.R

--- R/TEST2.R
+++ R/TEST2.R
@@ -1,19 +1,19 @@
 library(shiny)
 
-#generateBoard
-generateBoard = function(rows, cols, mines) {
+# Generate a matrix of a minesweeper grid
+generateBoard = function(nrows, ncols, nmines) {
   # Create empty board
-  board = matrix(0, nrow = rows, ncol = cols)
+  board = matrix(0, nrow = nrows, ncol = ncols)
 
   # Place mines randomly on the board
-  mine_positions = sample(1:(rows * cols), size = mines)
+  mine_positions = sample(1:(nrows * ncols), size = nmines)
   board[mine_positions] = -1
 
   # Calculate adjacent mine counts for each cell
-  for (r in 1:rows) {
-    for (c in 1:cols) {
+  for (r in 1:nrows) {
+    for (c in 1:ncols) {
       if (board[r, c] != -1) {
-        adjacent_cells <- board[max(1, r - 1):min(rows, r + 1), max(1, c - 1):min(cols, c + 1)]
+        adjacent_cells <- board[max(1, r - 1):min(nrows, r + 1), max(1, c - 1):min(ncols, c + 1)]
         board[r, c] <- sum(adjacent_cells == -1)
       }
     }
@@ -31,7 +31,7 @@ ui=fluidPage(
       actionButton("start","Start")
     ),
     mainPanel(
-      textOutput("mytext"),
+      verbatimTextOutput("mytext"),
       uiOutput("boardUI")
     )
   )
@@ -43,7 +43,7 @@ server = function(input,output,session){
     ncols = 10,
     nrows = 10,
     nmines = 10,
-    board = generateBoard(rows=10,cols=10,mines=10),
+    board = generateBoard(nrows=10,ncols=10,nmines=10),
     opened = matrix(FALSE,ncol=10,nrow=10),
     gameOver = FALSE,
     success = FALSE,
@@ -54,60 +54,62 @@ server = function(input,output,session){
     gameState$ncols = input$ncols
     gameState$nrows = input$nrows
     gameState$nmines = input$nmines
-    gameState$board = generateBoard(ncols=gameState$ncols,nrows=gameState$nrows,mines=gameState$nmines)
+    gameState$board = generateBoard(ncols=gameState$ncols,nrows=gameState$nrows,nmines=gameState$nmines)
     gameState$opened = matrix(FALSE,ncol=gameState$ncols,nrow=gameState$nrows)
     gameState$flags = matrix(FALSE,ncol=gameState$ncols,nrow=gameState$nrows)
     gameOver = FALSE
     gameState$success = FALSE
   })
-  #boardUI rendering using rows and cols
+  #boardUI rendering using nrows and ncols
   output$boardUI = renderUI({
-    lapply(1:gameState$rows, function(r){
-      fluidRow(lapply(1:gameState$cols, function(c){
-          actionButton(paste0("r",r,"c",c),
-                       if(gameState$opened[r,c]==FALSE){" "}
-                       else if(gameState$flags[r,c]){"f"}
-                       else{paste0(gameState$board[r,c])}
-          )
-        }))
+    grid = lapply(1:(gameState$nrows), function(r){
+      gridrow = lapply(1:(gameState$ncols), function(c){
+        actionButton(paste0("r",r,"c",c),
+                     if(gameState$opened[r,c]==FALSE){"_"}
+                     else if(gameState$flags[r,c]){"f"}
+                     else{paste0(gameState$board[r,c])}
+        )
+      })
+      tags$tr(tagList(gridrow))
     })
+    tags$table(tagList(grid))
   })
   # Success test
   observe({
-    if(sum(gameState$opened&(gameState$board!=-1))==gameState$ncols*gameState$nrows-gameState$mines){gameState$success=TRUE}
+    if((sum(gameState$opened&(gameState$board!=-1)))==(gameState$ncols*gameState$nrows-gameState$nmines)){gameState$success=TRUE}
   })
   # mytext initialization
-  output$mytext=renderUI(tags$text("Try your luck !"))
+  output$mytext=renderText("try your luck")
   # gameOver handling
   observeEvent(gameState$gameOver,{
-    if(gameState$gameOver){output$mytext=renderUI(tags$text("U lost !!"))}
-    else{output$mytext = renderUI(tags$text("go gogogo !"))}
+    if(gameState$gameOver){output$mytext=renderText(tags$text("U lost !!"))}
+    else{output$mytext = renderText(tags$text("go gogogo !"))}
   })
   # Success handling
   observeEvent(gameState$success,{
-    if(gameState$success){output$mytext=renderUI(tags$text("U won , start again with moooore mines!"))}
-    else{output$mytext=renderUI(tags$text("Yeeaaeaeeaeeaae gogogogogo !"))}
+    if(gameState$success){output$mytext=renderText("U won , start again with moooore mines!")}
+    else{output$mytext=renderText("Yeeaaeaeeaeeaae gogogogogo !")}
   })
   # Clicks handling
-  observe({
-    for(r in 1:gameState$nrows){
-      for(c in 1:gameState$ncols){
-        observeEvent(input[[paste0("r",r,"c",c)]],{
-          if(gameState$flags[r,c]==TRUE){return()}
-          else if(gameState$opened[r,c]==TRUE){return()}
-          else if(gameState$board[r,c]==-1){
-            gameState$opened[r,c]=TRUE
-            gameState$gameOver = TRUE
-          }
-          else {gameState$opened[r,c]=TRUE}
-        })
-      }
+observe({
+  for(r in 1:gameState$nrows){
+    for(c in 1:gameState$ncols){
+      observeEvent(input[[paste0("r",r,"c",c)]],{
+        if(gameState$flags[r,c]==TRUE){}
+        else if(gameState$opened[r,c]==TRUE){}
+        else if(gameState$board[r,c]==-1){
+          gameState$opened[r,c]=TRUE
+          gameState$gameOver = TRUE
+        }
+        else {gameState$opened[r,c]=TRUE}
+      })
     }
-  })
+  }
+})
+
+
 
 }
 
 shinyApp(ui,server)
-
-
 
